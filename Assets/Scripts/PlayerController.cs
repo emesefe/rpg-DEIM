a9f026d8eaafdc53c56674b9cc43f1977341015e
msantos98 <maria@frogames.es>
2022-10-11 08:31:05 +0200

    cambios inicio clase 11 oct

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,39 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
-    public const string HORIZONTAL = "Horizontal", VERTICAL = "Vertical";
+    public const string HORIZONTAL = "Horizontal", 
+        VERTICAL = "Vertical";
+    
     private float inputTol = 0.2f; // Input tolerance
     private float xInput, yInput;
     
+    private bool isWalking;
+    private Vector2 lastDirection;
+    private Animator _animator;
+    
+    private void Awake(){
+        _animator = GetComponent<Animator>();
+    }
+    
     void Update(){
         xInput = Input.GetAxisRaw(HORIZONTAL);
+        yInput = Input.GetAxisRaw(VERTICAL);
+        isWalking = false;
+        
         if (Mathf.Abs(xInput) > inputTol){
             Vector3 translation = new Vector3(xInput * speed * Time.deltaTime, 
                 0, 0);
             transform.Translate(translation);
+            isWalking = true;
+            lastDirection = new Vector2(xInput, 0);
         }
         
-        yInput = Input.GetAxisRaw(VERTICAL);
         if (Mathf.Abs(yInput) > inputTol){
             Vector3 translation = new Vector3( 0, 
                 yInput * speed * Time.deltaTime, 0);
             transform.Translate(translation);
+            isWalking = true;
+            lastDirection = new Vector2(0, yInput);
         }
     }
 }
