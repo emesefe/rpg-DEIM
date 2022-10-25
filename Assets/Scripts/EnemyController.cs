9c80d90c35fea9e20b5bf16d609b5610eb1b72af
msantos98 <maria@frogames.es>
2022-10-25 13:28:12 +0200

    25 oct

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 1f;
+    public Vector2 directionToMove;
+    
+    [Tooltip("Time the enemy takes between successive steps")]
+    [SerializeField] private float timeBetweenSteps;
+    // Time since the last step taken by the enemy
+    private float timeBetweenStepsCounter;
+    
+    [Tooltip("Time it takes for the enemy to take a step")]
+    [SerializeField] private float timeToMakeStep;
+    // Time the enemy has been taking a step
+    private float timeToMakeStepCounter;
+
+    private Rigidbody2D _rigidbody;
+    private bool isMoving;
+    
+    private void Start(){
+        _rigidbody = GetComponent<Rigidbody2D>();
+        timeBetweenStepsCounter = timeBetweenSteps;
+        timeToMakeStepCounter = timeToMakeStep;
+    }
+    
+    private void Update(){
+        if (isMoving)
+        {
+            timeToMakeStepCounter -= Time.deltaTime;
+            _rigidbody.velocity = speed * directionToMove;
+            if (timeToMakeStepCounter < 0)
+            {
+                isMoving = false;
+                timeBetweenStepsCounter = timeBetweenSteps;
+                _rigidbody.velocity = Vector2.zero;
+            } 
+        }
+        else
+        {
+            timeBetweenStepsCounter -= Time.deltaTime;
+            if (timeBetweenStepsCounter < 0)
+            {
+                isMoving = true;
+                timeToMakeStepCounter = timeToMakeStep;
+                directionToMove = new Vector2(
+                    Random.Range(-1, 2),
+                    Random.Range(-1, 2));
+            }
+        }
+    }
+}
