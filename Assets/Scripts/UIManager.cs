8e778ffb67860cd93b0a8293f94cec170c331815
msantos98 <maria@frogames.es>
2022-11-22 09:09:31 +0100

    22 nov

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -17,5 +17,31 @@ public class UIManager : MonoBehaviour
     public void ToggleInventory()
     {
         inventoryPanel.SetActive(!inventoryPanel.activeInHierarchy);
+        if (inventoryPanel.activeInHierarchy){
+            ClearInventory();
+            FillInventory();
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+    
+    public void FillInventory(){
+        WeaponManager weaponManager = FindObjectOfType<WeaponManager>();
+        List<GameObject> weapons = weaponManager.GetAllWeapons();
+        foreach(GameObject w in weapons){
+            Button button = Instantiate(weaponButton, inventoryPanel.transform);
+            button.onClick.AddListener(() => weaponManager.ChangeWeapon(
+                w.GetComponent<WeaponDamage>().index));
+            button.image.sprite = w.GetComponent<SpriteRenderer>().sprite;
+        }
+    }
+    
+    public void ClearInventory(){
+        foreach(Transform item in inventoryPanel.transform){
+            Destroy(item.gameObject);
+        }
     }
 }
